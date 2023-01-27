9f7aa1699247984006bf5e6c6a9364e5caea7236
Muhammad Fahad Umer <eng.fahadumer@gmail.com>
2023-01-27 17:04:01 +0500

    Ads Header and Theme

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import { ColorModeContext, useMode } from './theme';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import Topbar from './scenes/global/Topbar';
+import Sidebar from './scenes/global/Sidebar';
+import Dashboard from './scenes/dashboard';
+import Team from './scenes/team';
+import Invoices from './scenes/invoices';
+import Contacts from './scenes/contacts';
+import bar from './scenes/bar';
+import form from './scenes/form';
+import line from './scenes/line';
+import pie from './scenes/pie';
+import FAQ from './scenes/faq';
+import Geography from './scenes/geography';
+
 function App() {
-  return <div className="App">Hello World</div>;
+  const [theme, colorMode] = useMode();
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="App">
+          <main className="content">
+            <Topbar />
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
 export default App;
